e608e803332f7f356400593c4f07dc205c8fb9f3
johnatanSO <jonystrit@gmail.com>
2023-07-24 17:17:45 -0300

    :sparkles: [FEAT] - Criando botões e adicionando imagens.

--- frontend/src/screens/Student/index.tsx
+++ frontend/src/screens/Student/index.tsx
@@ -1,26 +1,49 @@
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import style from './Student.module.scss'
+import Image from 'next/image'
+import studentImage from '../../../public/assets/student.png'
+import notesImage from '../../../public/assets/notepad.png'
+import warningImage from '../../../public/assets/warning.png'
+import timetableImage from '../../../public/assets/timetable.png'
 
 export function Student() {
   return (
     <>
       <div className={style.avatarContainer}>
-        <FontAwesomeIcon className={style.icon} icon={faUser} />
+        <div className={style.imageContainer}>
+          <Image
+            src={studentImage}
+            alt="student icon"
+            className={style.image}
+          />
+        </div>
         <h3>nome</h3>
       </div>
 
       <ul className={style.buttonsContainer}>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image src={notesImage} alt="Notas icon" className={style.image} />
+          </div>
           <h4>Notas</h4>
         </li>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image
+              src={warningImage}
+              alt="Advertências icon"
+              className={style.image}
+            />
+          </div>
           <h4>Advertências</h4>
         </li>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image
+              src={timetableImage}
+              alt="Faltas icon"
+              className={style.image}
+            />
+          </div>
           <h4>Faltas</h4>
         </li>
       </ul>
--- frontend/src/screens/Teacher/index.tsx
+++ frontend/src/screens/Teacher/index.tsx
@@ -1,30 +1,60 @@
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import style from './Teacher.module.scss'
+import Image from 'next/image'
+import teacherImage from '../../../public/assets/teacher.png'
+import notesImage from '../../../public/assets/notepad.png'
+import warningImage from '../../../public/assets/warning.png'
+import timetableImage from '../../../public/assets/timetable.png'
+import registerImage from '../../../public/assets/register.png'
 
 export function Teacher() {
   return (
     <>
       <div className={style.avatarContainer}>
-        <FontAwesomeIcon className={style.icon} icon={faUser} />
+        <div className={style.imageContainer}>
+          <Image
+            src={teacherImage}
+            alt="teacher icon"
+            className={style.image}
+          />
+        </div>
         <h3>nome</h3>
       </div>
 
       <ul className={style.buttonsContainer}>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image src={notesImage} alt="Notas icon" className={style.image} />
+          </div>
           <h4>Notas</h4>
         </li>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image
+              src={warningImage}
+              alt="Advertências icon"
+              className={style.image}
+            />
+          </div>
           <h4>Advertências</h4>
         </li>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image
+              src={timetableImage}
+              alt="Faltas icon"
+              className={style.image}
+            />
+          </div>
           <h4>Faltas</h4>
         </li>
         <li>
-          <FontAwesomeIcon className={style.icon} icon={faUser} />
+          <div className={style.imageContainer}>
+            <Image
+              src={registerImage}
+              alt="Cadastro icon"
+              className={style.image}
+            />
+          </div>
           <h4>Gerenciar alunos</h4>
         </li>
       </ul>

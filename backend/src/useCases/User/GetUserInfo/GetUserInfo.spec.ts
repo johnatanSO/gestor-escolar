99be715e3c21119ae638a56e74d45f8aa50d5507
johnatanSO <jonystrit@gmail.com>
2023-09-20 17:47:07 -0300

    :tube_test: [TESTS] - Testes de busca de usuário.

--- backend/src/useCases/User/GetUserInfo/GetUserInfo.spec.ts
+++ backend/src/useCases/User/GetUserInfo/GetUserInfo.spec.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose'
 import { AppError } from '../../../errors/AppError'
 import { MockUsersRepository } from './../../../repositories/Users/MockUsersRepository'
 import { GetUserInfoService } from './GetUserInfoService.service'
@@ -16,6 +17,14 @@ describe('Get user info', () => {
   it('should not be able search user info if idUser not sent', async () => {
     await expect(async () => {
       await getUserInfoService.execute(undefined)
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able search user info if idUser is from invalid user', async () => {
+    await expect(async () => {
+      const idUser = new Types.ObjectId()
+
+      await getUserInfoService.execute(idUser.toString())
     }).rejects.toBeInstanceOf(AppError)
   })
 })

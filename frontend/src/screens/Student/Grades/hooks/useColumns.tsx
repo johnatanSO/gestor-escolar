19c28d684d9d566c02280cd6e0c2533767d8d6a2
johnatanSO <jonystrit@gmail.com>
2024-01-26 15:19:15 -0300

    :bricks: [BUILD] - Corrigindo erro de build.

--- frontend/src/screens/Student/Grades/hooks/useColumns.tsx
+++ frontend/src/screens/Student/Grades/hooks/useColumns.tsx
@@ -11,35 +11,35 @@ export function useColumns() {
     {
       headerName: 'Código',
       field: 'subjectCode',
-      valueFormatter: (params: CellFunctionParams) => params.value,
+      valueFormatter: (params: CellFunctionParams<any>) => params.value,
     },
     {
       headerName: 'Nome da disciplina',
       field: 'subjectName',
-      valueFormatter: (params: CellFunctionParams) => params.value || '--',
+      valueFormatter: (params: CellFunctionParams<any>) => params.value || '--',
     },
     {
       headerName: 'Nota 1',
       field: 'subjectGrades',
-      cellClass: (params: CellFunctionParams) =>
+      cellClass: (params: CellFunctionParams<any>) =>
         getAvarageStatus(params.value.firstGrade),
-      valueFormatter: (params: CellFunctionParams) =>
+      valueFormatter: (params: CellFunctionParams<any>) =>
         params?.value?.firstGrade?.toFixed(2),
     },
     {
       headerName: 'Nota 2',
       field: 'subjectGrades',
-      cellClass: (params: CellFunctionParams) =>
+      cellClass: (params: CellFunctionParams<any>) =>
         getAvarageStatus(params.value.secondGrade),
-      valueFormatter: (params: CellFunctionParams) =>
+      valueFormatter: (params: CellFunctionParams<any>) =>
         params?.value?.secondGrade?.toFixed(2),
     },
     {
       headerName: 'Total',
       field: 'subjectGrades',
-      cellClass: (params: CellFunctionParams) =>
+      cellClass: (params: CellFunctionParams<any>) =>
         `${style.totalGrade} ${getAvarageStatus(params.value.totalGrades)}`,
-      valueFormatter: (params: CellFunctionParams) =>
+      valueFormatter: (params: CellFunctionParams<any>) =>
         params?.value?.totalGrades?.toFixed(2),
     },
   ]

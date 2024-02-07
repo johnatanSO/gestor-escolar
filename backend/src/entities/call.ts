550fb0a7ce52b6df06c1cfd997688a3af6b56b73
johnatanSO <jonystrit@gmail.com>
2024-02-07 15:40:01 -0300

    :sparkles: [FEAT] - Entitie de chamada.

---
+++ backend/src/entities/call.ts
@@ -0,0 +1,23 @@
+import mongoose, { ObjectId } from 'mongoose'
+
+export interface ICall {
+  _id: ObjectId
+  createdAt: Date
+  date: Date
+}
+
+const callSchema = new mongoose.Schema({
+  createdAt: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now },
+  presences: [
+    {
+      student: {
+        type: 'ObjectId',
+        ref: 'User',
+      },
+      present: { type: Boolean, default: false },
+    },
+  ],
+})
+
+export const CallModel = mongoose.model<ICall>('Call', callSchema)

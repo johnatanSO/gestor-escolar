ac61892be566648f041b630f50b3827d567bfe6e
johnatanSO <jonystrit@gmail.com>
2024-04-19 16:51:07 -0300

    :sparkles: [REPLACE] - Alterando o storage da AWS S3 para o Firebase Storage.

--- backend/src/shared/containers/providers/index.ts
+++ backend/src/shared/containers/providers/index.ts
@@ -1,9 +1,10 @@
 import { container } from 'tsyringe'
 
-import { S3StorageProvider } from './StorageProvider/S3StorageProvider'
+// import { S3StorageProvider } from './StorageProvider/S3StorageProvider'
 import { IStorageProvider } from './StorageProvider/IStorageProvider'
+import { FirebaseProvider } from './StorageProvider/FirebaseProvider'
 
 container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  S3StorageProvider,
+  'FirebaseProvider',
+  FirebaseProvider,
 )
